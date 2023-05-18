589c9b0d47c2fb971e2f6451e0f7f39b2b8eabf1
Mattthew R Kasun <mkasun@gmail.com>
2023-05-18 17:35:27 +0000

    v0.20.0

--- Casks/nmctl.rb
+++ Casks/nmctl.rb
@@ -1,11 +1,11 @@
 cask "nmctl" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  version "0.19.0"
+  version "0.20.0"
 
   if Hardware::CPU.intel?
-    sha256 "17136540b533125c77ff2cd47298b508322ba81ad18291ddc027eb97be3c7e12"
+    sha256 "ba4a0954e8ae8f46c1a9fe865caa062ed7e131b60237bf9a3a49a794b4641076"
   else
-    sha256 "9d00b7941b9b747d793496e8bc500b661161bfed582b9d41e91ac49020f54084"
+    sha256 "14dee87944c3188b15fc7401f3c1459cc4e75e8f7d310777dff962cbeb66e809"
   end
 
   url "https://fileserver.netmaker.org/v#{version}/darwin/nmctl-#{arch}.tgz",
