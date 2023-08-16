7aedae0fb20d718f392c685134c0dff2f8cf3286
Mattthew R Kasun <mkasun@gmail.com>
2023-08-16 18:10:32 +0000

    version 0.20.6

--- Casks/nmctl.rb
+++ Casks/nmctl.rb
@@ -1,11 +1,11 @@
 cask "nmctl" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  version "0.20.5"
+  version "0.20.6"
 
   if Hardware::CPU.intel?
-    sha256 "6b66cac6d93077f7e3358da14a272ce98769afff5d0ac0202d019b785bedab3f"
+    sha256 "b6bc9b5fd5908d8faa69bd4ceb1ea7139f77b61e4140968e9bd5878edc348098"
   else
-    sha256 "5e3bc90296061a13cc35649ec3cf5e600b55b443eaaaf03345a7da126941a5a9"
+    sha256 "0eacaf9dcae915f8f55129614b9da03e93c058ef2eeed5f5a5ca2665546b1154"
   end
 
   url "https://fileserver.netmaker.org/releases/download/v#{version}/nmctl-#{arch}.tgz",
