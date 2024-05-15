b7cb3262817a19bc731532fd7d94180735030f16
theguy951357 <crispspiceguitar@gmail.com>
2024-05-15 17:27:50 +0000

    new version

--- Casks/nmctl.rb
+++ Casks/nmctl.rb
@@ -1,11 +1,11 @@
 cask "nmctl" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  version "0.24.0"
+  version "0.24.1"
 
   if Hardware::CPU.intel?
-    sha256 "0661828b708e71ab2bd36f00109f70c6fe9b549bb12886d616a0c1b6723da90b"
+    sha256 "9a115c803231248378f9d24e86b2fa136b6a5260432d78fac102dc621a83c15a"
   else
-    sha256 "9d0cf341f256daf6eca1070efed130026f6fa25b5680f1ac5773e16476267a91"
+    sha256 "1afbed9b7ca04d7d3ea0a5be7cbd1e30517ab2c613acc3c7d70f0a27b68ff582"
   end
 
   url "https://fileserver.netmaker.org/releases/download/v#{version}/nmctl-#{arch}.tgz",
